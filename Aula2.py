192d3a486456650a1f15b69ed3192195e9ab69aa
LeonTrevis <leonardotrevisan18@gmail.com>
2025-03-17 21:02:42 -0300

    Meu primeiro programa em python, codigos adicionados

--- Aula2.py
+++ Aula2.py
@@ -1,6 +1,28 @@
 # Aula2 do curso de python (eng. mecatronica 2025)
-print("Hello World")
-a = 2
-b = 3
-print(a+b)
+
+# 1.Saída de dados
+print("Hello World!")
+
+# 2. Exercicio solicitar o nome do usuario
+print("Qual o seu nome?")
+nome = str(input())
+print("Seu nome é:", nome)
+
+# 3. Exercicio solicitar numeros para somar, junto com o nome do usuario
+print(nome, "escolha 3 valores a serem somados")
+a1 = float(input())
+b1 = float(input())
+c1 = float(input())
+soma = (a1+b1+c1)
+print(nome, "a soma de", a1, b1, c1, "é de:", f"{soma:.2f}")
+
+# 4. Exercicio de media + nome do usuario
+
+print(nome, "entre com os valores das 3 notas")
+a2 = float(input())
+b2 = float(input())
+c2 = float(input())
+media = ((a2+b2+c2)/3)
+
+print(nome, "a media das notas", a2, b2, c2, "é de:", f"{media:.2f}")
 
