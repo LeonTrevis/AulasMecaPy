326c9de2e38681d7032ffcf3767a95e26178e2c8
LeonTrevis <leonardotrevisan18@gmail.com>
2025-08-04 21:03:16 -0300

    g

---
+++ aula5.py
@@ -0,0 +1,54 @@
+# montagem de listas dos alunos de uma sala
+# nomes_alunos=[]
+# print(nomes_alunos)
+# print(len(nomes_alunos))
+# nomes_alunos=["Leonardo", "Guilherme", "Vinicius", "Giovanna"]
+# print(nomes_alunos)
+# print(len(nomes_alunos))
+# nomes_alunos[0] = "Leonardo Martins Trevisan"
+# print(nomes_alunos)
+
+#montagem de lista de notas para soma de média
+# notas=[6,4,8,7,9,7]
+# soma=0
+# x=0
+#
+# while x< len(notas):
+#     soma += notas[x]
+#     print(soma)
+#     x+= 1
+# print(f"Média ={(soma/len(notas)):.2f}")
+
+#montagem de lista vazia para inserção de nota pelo usuario
+# notas=[0,0,0,0,0,0,0]
+# soma=0
+# x=0
+#
+# while x<len(notas):
+#     notas[x]=float(input(f"Nota{x+1}="))
+#     soma+=notas[x]
+#     print(soma)
+#     x+=1
+#
+# print(f"Média = {(soma/x):.2f}")
+
+
+# #copiando uma lista para fazer um backup
+# nomes_alunos=[]
+# print(nomes_alunos)
+# print(len(nomes_alunos))
+# nomes_alunos=["Leonardo", "Guilherme", "Vinicius", "Giovanna"]
+# print(nomes_alunos)
+# print(len(nomes_alunos))
+# nomes_alunos[0] = "Leonardo Martins Trevisan"
+# print(nomes_alunos)
+#
+# nomes_alunos_back=nomes_alunos[:]
+# print(nomes_alunos_back)
+#
+# nomes_alunos_back[1]="Guilherme Gama"
+# nomes_alunos_back[2]="Vinicius Gonçalves"
+# nomes_alunos_back[3]="Giovanna Carmona"
+#
+# print(nomes_alunos)
+# print(nomes_alunos_back)
